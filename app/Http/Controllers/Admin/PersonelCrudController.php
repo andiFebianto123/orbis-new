dd37321259765d1c5c82b19237ba7ac37b9f4b33
danihadi <detal31392@gmail.com>
2021-04-15 22:28:02 +0700

    use Hash

--- app/Http/Controllers/Admin/PersonelCrudController.php
+++ app/Http/Controllers/Admin/PersonelCrudController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\PersonelRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
-use App\Models\SpecialRole;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * Class PersonelCrudController
