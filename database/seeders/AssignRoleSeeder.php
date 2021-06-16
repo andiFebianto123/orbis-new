5c7c578008bcfeb60f6519b693e30f15e876b225
Nico Desmonda <rect.nicodesmonda@gmail.com>
2021-06-16 20:39:45 +0700

    Fix Seeder 5

--- database/seeders/AssignRoleSeeder.php
+++ database/seeders/AssignRoleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\ModelHasRole;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
 
@@ -16,14 +17,16 @@ class AssignRoleSeeder extends Seeder
     public function run()
     {
         Schema::disableForeignKeyConstraints();
+
+        $role_id = Role::where('name', 'Super Admin')->first();
         if(ModelHasRole::where('model_id', 1)->count() != 0){
             ModelHasRole::where('model_id', 1)->update([
-                'role_id' => 3
+                'role_id' => $role_id ?? 3
             ]);
         }
         else{
             ModelHasRole::create([
-                'role_id' => 3,
+                'role_id' => $role_id ?? 3,
                 'model_type' => 'App\Models\User',
                 'model_id' => 1
             ]);
