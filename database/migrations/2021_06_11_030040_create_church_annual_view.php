ee41ba3d74c57f325c3b49c1b50de11165b231ef
Nico Desmonda <rect.nicodesmonda@gmail.com>
2021-06-16 19:20:21 +0700

    Fix Migration View

--- database/migrations/2021_06_11_030040_create_church_annual_view.php
+++ database/migrations/2021_06_11_030040_create_church_annual_view.php
@@ -15,9 +15,12 @@ class CreateChurchAnnualView extends Migration
     {
         DB::statement("DROP VIEW IF EXISTS church_annual_views");
         DB::statement("CREATE VIEW church_annual_views AS 
-        SELECT row_number() OVER (ORDER BY year) AS id, count(founded_on) AS total , YEAR(founded_on) AS year FROM churches 
-        WHERE founded_on IS NOT NULL
-        GROUP BY year");
+        SELECT @row_number:=@row_number+1 as id, ct.* from(
+            SELECT count(founded_on) AS total , YEAR(founded_on) AS year FROM churches, 
+            (SELECT @row_number:=0) AS t
+            WHERE founded_on IS NOT NULL
+            GROUP BY year)
+        as ct");
     }
 
     /**
