0f0a5a394ae012f55fccf34e6a7e13a86e15aea8
Kevin Djajadinata <kevin@rectmedia.com>
2021-11-26 18:27:29 +0700

    remove print from SyncUserToMailchimp

--- app/Console/Commands/SyncUserToMailchimp.php
+++ app/Console/Commands/SyncUserToMailchimp.php
@@ -101,7 +101,7 @@ class SyncUserToMailchimp extends Command
                 }
             }
             while($countMembers > 0);
-            print_r($emails);
+            // print_r($emails);
 
             $memberTags = [];
             $memberKeys = [];
@@ -149,7 +149,6 @@ class SyncUserToMailchimp extends Command
                 if(count($members) > 0){
                    // print_r($members);
                    $response = $mailchimp->lists->batchListMembers($listId, ["members" => $members, "update_existing" => true]);
-                   print_r($response->errors);
                    foreach($response->errors as $error){
                         unset($memberTags[md5($error->email_address)]);
                    }
