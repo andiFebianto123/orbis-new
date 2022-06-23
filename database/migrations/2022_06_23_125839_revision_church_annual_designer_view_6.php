e338c7abfee524c27e145976be15fc7f55ea5cbc
danihadi <detal31392@gmail.com>
2022-06-23 18:15:44 +0700

    rename revision view

--- database/migrations/2022_06_23_125839_revision_church_annual_designer_view_6.php
+++ database/migrations/2022_06_23_125839_revision_church_annual_designer_view_6.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class RevisionChurchAnnualDesignerView5 extends Migration
+class RevisionChurchAnnualDesignerView6 extends Migration
 {
     /**
      * Run the migrations.
