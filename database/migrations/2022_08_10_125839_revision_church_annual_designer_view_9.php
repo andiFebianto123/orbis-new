784610503870a9a8e432671721cbf2aeefcc40ff
danihadi <detal31392@gmail.com>
2022-08-10 18:06:22 +0700

    rename migrateion

--- database/migrations/2022_08_10_125839_revision_church_annual_designer_view_9.php
+++ database/migrations/2022_08_10_125839_revision_church_annual_designer_view_9.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class RevisionChurchAnnualDesignerView8 extends Migration
+class RevisionChurchAnnualDesignerView9 extends Migration
 {
     /**
      * Run the migrations.
