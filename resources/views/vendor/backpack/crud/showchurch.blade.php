cff1aaede5106bf17a87d67dc88c411104528a29
Andi <andi@rectmedia.com>
2022-08-10 11:07:43 +0700

    hilangkan order di church

--- resources/views/vendor/backpack/crud/showchurch.blade.php
+++ resources/views/vendor/backpack/crud/showchurch.blade.php
@@ -166,7 +166,7 @@
 								</tr>
 								<tr>
 									<td>Task Color</td>
-									<td> : <span style="width:20px; display:block; height: 20px; background-color:{{ $entry->task_color }};"></span></td>
+									<td> : <div style="width: 100px; display: inline-flex; height: 30px; margin: -10px 0px; background-color:{{ $entry->task_color }};"></div></td>
 								</tr>
 								<tr> 
 									<td>Latitude</td>
